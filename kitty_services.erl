2438f1ae91ac5f7dd3878b71597d795864ce1b3e
fahmi_tok <fahmitok123@gmail.com>
2017-12-15 20:23:01 +0700

    enrich cat model with jobs, implement register job mechanism

--- kitty_services.erl
+++ kitty_services.erl
@@ -2,10 +2,11 @@
 -module(kitty_services).
 
 -export([start_link/0, order_cat/4, return_cat/2, close_shop/1, 
-        start_deceased_cat_server/0, show_deceased_cat_list/1, register_deceased_cat/5]).
+        start_deceased_cat_server/0, show_deceased_cat_list/1, register_deceased_cat/5,
+        assign_work/3, retire/2, add_cat/4, browse_cat_with_job/1]).
 -export([init/1, handle_call/3, handle_cast/2, tukar_kucing/4]).
 
--record(cat, {name, color=black, description}).
+-record(cat, {name, color=black, description, job=unemployed}).
 -record(deceased_cat, {name, date, cause}).
 
 %%% Client API
@@ -40,6 +41,23 @@ close_shop(Pid) ->
 %% Synchronous call untuk fungsi tukar_kucing
 tukar_kucing(Pid, Name, Color, Description) ->
     my_server:call(Pid, {tukar, Name, Color, Description}).
+
+%% Synchronous call untuk fungsi assign_work
+assign_work(Pid, Name, Job) ->
+    my_server:call(Pid, {work, Name, Job}).
+
+%% Synchronous call untuk fungsi retire
+retire(Pid, Name) ->
+    my_server:call(Pid, {retire, Name}).
+
+
+%% Synchronous call untuk fungsi browse_cat_with_job
+browse_cat_with_job(Pid) ->
+    my_server:call(Pid, browse).
+
+%% Synchronous call untuk fungsi add_cat
+add_cat(Pid, Name, Color, Description) ->
+    my_server:call(Pid, {add_cat, Name, Color, Description}).
 
 %%% Server functions
 init([]) -> []. %% no treatment of info here!
@@ -87,7 +105,56 @@ handle_call({decease, Name, Date, Cause}, From, DeceasedCats) ->
         _ ->
             my_server:reply(From, {error, "can't die more than once"}),
             DeceasedCats
-    end.
+    end;
+
+%%%% Server functions for assigning work to respective cat
+handle_call({work, Name, Job}, From, Cats) ->
+    case lists:filter(fun(Cat) -> Cat#cat.name == Name end, Cats) of
+        [] ->
+            my_server:reply(From, {error, "no cat with respective name"}),
+            Cats;
+        _ ->
+            my_server:reply(From, {ok, assigned}),
+            lists:map(
+                fun(Cat) ->
+                    case Cat#cat.name == Name of
+                        true -> make_cat_with_job(Cat#cat.name, Cat#cat.color, Cat#cat.description, Job);
+                        false -> Cat
+                    end
+                end,
+                Cats
+                )
+    end;
+
+%%%% Server functions for retiring a respective cat
+handle_call({retire, Name}, From, Cats) ->
+    case lists:filter(fun(Cat) -> Cat#cat.name == Name end, Cats) of
+        [] ->
+            my_server:reply(From, {error, "no cat with respective name"}),
+            Cats;
+        _ ->
+            my_server:reply(From, {ok, retired}),
+            lists:map(
+                fun(Cat) ->
+                    case Cat#cat.name == Name of
+                        true -> make_cat_with_job(Cat#cat.name, Cat#cat.color, Cat#cat.description, retirement);
+                        false -> Cat
+                    end
+                end,
+                Cats
+                )
+    end;
+
+%%%% Server functions for showing all cats
+handle_call(browse, From, Cats) ->
+    my_server:reply(From, {ok, [{Cat#cat.name, Cat#cat.job} || Cat <- Cats]}),
+    Cats;
+
+handle_call({add_cat, Name, Color, Description}, From, Cats) ->
+
+    my_server:reply(From, {ok, add_cat}),
+    Cats ++ [make_cat(Name, Color, Description)].
+
 
 handle_cast({return, Cat = #cat{}}, Cats) ->
     [Cat|Cats];
@@ -101,7 +168,11 @@ make_cat(Name, Col, Desc) ->
 
 make_deceased_cat(Name, Date, Cause) ->
     #deceased_cat{name=Name, date=Date, cause=Cause}.
-    
+
+
+make_cat_with_job(Name, Col, Desc, Job) ->
+    #cat{name=Name, color=Col, description=Desc, job=Job}.
+
 terminate(Cats) ->
     [io:format("~p was set free.~n",[C#cat.name]) || C <- Cats],
     exit(normal).

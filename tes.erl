e99d90d7c83ab278ca5c1aaaf525118932d3fb74
faqrulan <faqrulans@gmail.com>
2017-12-16 01:02:30 +0700

    delete tes file for debug

--- tes.erl
+++
@@ -1,9 +0,0 @@
--module(tes).
-
--record(cat, {name, color=black, description}).
-
--export([tes_fun/1]).
-
-tes_fun(Cat = #cat{}) ->
-	Name = Cat#cat.name,
-	lists:concat([Name, " bersih kembali, kembalian anda ", 5000]).

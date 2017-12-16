d2b3e21536068cf366a66343e77e58013fbd01d6
Cenna <avcwisesa@gmail.com>
2017-12-16 22:43:14 +0700

    add working hour guard to server loop

--- my_server.erl
+++ my_server.erl
@@ -1,5 +1,14 @@
 -module(my_server).
 -export([start/2, start_link/2, call/2, cast/2, reply/2]).
+-export([make_hour/2, make_hour/0]).
+
+-record(work, {starth, endh}).
+
+make_hour() ->
+	make_hour(9, 17).
+
+make_hour(Start, End) -> 
+	#work{starth=Start, endh=End}.
 
 %%% Public API
 start(Module, InitialState) ->
@@ -30,13 +39,37 @@ reply({Pid, Ref}, Reply) ->
 
 %%% Private stuff
 init(Module, InitialState) ->
-    loop(Module, Module:init(InitialState)).
+    loop(Module, Module:init(InitialState), make_hour()).
 
-loop(Module, State) ->
+
+loop(Module, State, Work_Hour) ->
     receive
         {async, Msg} ->
-             loop(Module, Module:handle_cast(Msg, State));
+        	case is_work_hour(Work_Hour) of
+        		true	->
+        			loop(Module, Module:handle_cast(Msg, State), Work_Hour);
+            	false	->
+            		not_work_hour(Work_Hour),
+            		loop(Module, State, Work_Hour)
+            end;
+
         {sync, Pid, Ref, Msg} ->
-             loop(Module, Module:handle_call(Msg, {Pid, Ref}, State))
+        	case is_work_hour(Work_Hour) of
+        		true	->
+        			loop(Module, Module:handle_call(Msg, {Pid, Ref}, State), Work_Hour);
+        		false	->
+					reply({Pid, Ref}, not_work_hour(Work_Hour)),
+            		loop(Module, State, Work_Hour)
+            end
     end.
 
+is_work_hour(Work_Hour) ->
+	{{_,_,_},{Hour,_,_}} = erlang:localtime(),
+	Not_early = (Hour >= Work_Hour#work.starth),
+	Not_late = Hour < Work_Hour#work.endh,
+	Not_early and Not_late.
+
+not_work_hour(Work_Hour) ->
+	io:format("Petugas sedang beristirahat, coba datang lagi pada jam kerja.~nJam kerja: ~p s/d ~p~n",
+		[Work_Hour#work.starth, Work_Hour#work.endh]),
+	ok.

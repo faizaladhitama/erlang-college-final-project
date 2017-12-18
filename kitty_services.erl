bc25019abccb40f3163feffc1928b117be843db4
Cenna <avcwisesa@gmail.com>
2017-12-18 18:17:51 +0700

    add guard for change_working_hour parameter

--- kitty_services.erl
+++ kitty_services.erl
@@ -45,9 +45,13 @@ register_deceased_cat(Pid1, Pid2, Name, Date = {DD, MM, YY}, Cause) ->
     end.
 
 %% Call to change server's working hour
-change_working_hour(Pid, Start, End) ->
-	my_server:change_working_hour(Pid, Start, End).    
-
+change_working_hour(Pid, Start, End) when is_integer(Start), is_integer(End)->
+    if End =< Start    ->
+            erlang:error("Start begins before End")
+	;  End > Start     ->
+            my_server:change_working_hour(Pid, Start, End)
+    end.
+    
 %% Synchronous call
 close_shop(Pid) ->
     my_server:call(Pid, terminate).
